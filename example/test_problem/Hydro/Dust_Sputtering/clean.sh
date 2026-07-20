34728547f3f0313e8bb49d00d2bcaf2f9ed05660
Che-Wei <tommy@spock00.gpucluster.calab>
2026-07-20 15:04:39 +0800

    delete old files

--- example/test_problem/Hydro/Dust_Sputtering/clean.sh
+++
@@ -1,4 +0,0 @@
-rm -f Record__Note Record__Timing Record__TimeStep Record__PatchCount Record__Dump Record__MemInfo Record__L1Err \
-      Record__Conservation Data* stderr stdout log Record__Performance Record__TimingMPI_* \
-      Record__ParticleCount Record__User Patch_* Record__NCorrUnphy FailedPatchGroup* *.pyc Record__LoadBalance \
-      GRACKLE_INFO
--- example/test_problem/Hydro/Dust_Sputtering/generate_make.sh
+++
@@ -1,8 +0,0 @@
-# This script should run in the same directory as configure.py
-
-PYTHON=python3
-
-${PYTHON} configure.py --mpi=true --hdf5=true --gpu=true --model=HYDRO \
-                       --passive=2 --double=true \
-                       --grackle=true "$@"
-
